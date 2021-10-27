cdfcdbc101b7e1893b663b39f160fbb5ab6da8e5
wang <wan@Xiaomi-BookPro.local>
2021-10-27 12:51:47 +0800

    VPN client installation script for macOS

---
+++ Formula/tunnelblick.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Tunnelblick < Formula
+  desc "这是一个用于访问内网的VPN客户端！"
+  homepage "https://blog.csdn.net/Wangjiachenga"
+  url "https://github.com/wobushishanpao/software/blob/master/Tunnelblick-3.8.4.tar.gz"
+  sha256 "3a64df96fb6891847494df76f73ccd136ae3ac97364d87d0f5e1245c3a078596"
+ 
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test Tunnelblick`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
