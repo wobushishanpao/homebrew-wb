10a45bd6ac0c9a8787a1a17d88c14c3acaaea0f3
System Administrator <root@Xiaomi-BookPro.local>
2021-11-16 11:38:34 +0800

    wget install script

--- Formula/wget.rb
+++ Formula/wget.rb
@@ -14,9 +14,9 @@ class Wget < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    #system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    system "./configure", "--prefix=/usr/local/Cellar/wget/1.21.2"
+    system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
 
